b3f00586c225d317c5c2ad499a71b2bc2074de2a
annikafsjf <annikadgh@gmail.com>
2021-04-08 15:49:04 +0200

    help 2

---
+++ polaris_the_alien/alien.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Alien:
+    def __init__(self,speed,surface):
+        self.speed = speed
+        self.surface = surface
+
+    def draw_self(self):
+        alien = pygame.image.load('./data/alien_drawing.png')
+        
