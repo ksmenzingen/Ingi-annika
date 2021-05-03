8503771ccd2022b2aec90b5f4d82cdaebdad2f5f
Ingi74 <askur.valgeirsson@edu.ksmenzingen.ch>
2021-05-03 08:24:31 +0200

    updated background

--- polaris_the_alien/background.py
+++ polaris_the_alien/background.py
@@ -7,14 +7,11 @@ class Background:
         self.surface = pygame.display.set_mode((self.width, self.height))
 
     def draw_self(self):
-<<<<<<< HEAD
-=======
         x0 = 0
         y0 = 0
         cellSize = self.width/20
         columns = 20
         rows = columns
->>>>>>> 2949e0d4b4148ebe527658ecba1f17e567d8eef2
         self.surface.fill((8, 0, 70))
         pygame.display.update()
         for i in range(0,columns):
