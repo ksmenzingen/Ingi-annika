a3d2ad2478beee90b65d59e7727efffb46a0d6d3
Ingi74 <askur.valgeirsson@edu.ksmenzingen.ch>
2021-05-29 04:15:29 +0200

    merge

--- polaris_the_alien/main.py
+++ polaris_the_alien/main.py
@@ -11,33 +11,24 @@ delay = 200
 game_over = False
 background = Background(800, 800)
 alien = Alien(100,"alien_drawing2.png",(background.height*0.5-50),100)
-obstacles = Obstacles(background,(200,0,0),100)
+obstacles = Obstacles(background,(200,0,0),100,random.randint(50,125),random.randint(100,300))
 
 #start game
 while not game_over:
     #background.draw_self()
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                game_over = True
-
-        while event.type == pygame.KEYDOWN:
-
+          while event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                alien.move(3)
+                alien.coord_y += 0.5
                 background.draw_self(alien,obstacles)
-
-
-            if event.key == pygame.K_UP:
-                alien.move(-3)
-                background.draw_self(alien,obstacles)
-
-            for event in pygame.event.get():
                 if event.type == pygame.KEYUP:
                     break
 
+            if event.key == pygame.K_UP:
+                alien.coord_y -= 5
+                background.draw_self(alien,obstacles)
 
-        if event.type == pygame.QUIT:
+          if event.type == pygame.QUIT:
             game_over = True
             break
     #obstacles.generate()
